af75c585ff356e7589f20c614bc3d404bcca26b4
Isak Erikson Winbäck <erikson.winback.isak@elev.ga.ntig.se>
2022-03-09 14:58:19 +0100

    javascript

---
+++ webb2/ö.js
@@ -0,0 +1,14 @@
+"use strict";
+
+let hpv = document.querySelector("hpv");
+let hph = document.querySelector("hph");
+let hpvjs = 100;
+let hphjs = 100;
+
+uppdateraHP();
+
+alert("du har 5.000.000.000 virus");
+
+console.log("srdxfctgvyubjhkn");
+
+function uppdateraHP() {}
