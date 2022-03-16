2631c0a6fdaa009b1d5a82a3ebf8ea4b922300e8
Isak Erikson Winbäck <erikson.winback.isak@elev.ga.ntig.se>
2022-03-16 14:25:13 +0100

    4e6rf5t7guyihu

--- webb2/ö.js
+++ webb2/ö.js
@@ -25,37 +25,25 @@ const h3 = document.getElementById("h3");
 const h4 = document.getElementById("h4");
 const h5 = document.getElementById("h5");
 
-while (c == 0) {
-  while (turn == 1) {
-    v.addEventListener("click", function (e) {
-      hphjs = hphjs - skadav;
-      uppdateraHP();
-    });
-    turn = 2;
-    v2.addEventListener("click", function (q) {
-      hphjs = hphjs - 5;
-      uppdateraHP();
-    });
-    turn = 2;
-  }
-  while (turn == 2) {
-    h.addEventListener("click", function (w) {
-      hpvjs = hpvjs - skadah;
-      uppdateraHP();
-    });
-    turn = 1;
-
-    h2.addEventListener("click", function (r) {
-      hpvjs = hpvjs - 5;
-      uppdateraHP();
-    });
-    turn = 1;
-  }
+if (turn == 1) {
+  v.addEventListener("click", function (e) {
+    hphjs = hphjs - skadav;
+    uppdateraHP();
+  });
+  turn = 2;
+}
+if (turn == 2) {
+  h.addEventListener("click", function (e) {
+    hpvjs = hpvjs - skadah;
+    uppdateraHP();
+  });
+  turn = 2;
 }
 
-alert("du har 5.000.000.000 virus");
+alert("du har 5000.000.000 virus");
 
-console.log("srdxfctgvyubjhkn");
+console.log(v);
+console.log("ska förändras?");
 
 function uppdateraHP() {
   document.getElementById("hpv").innerHTML = hpvjs;
